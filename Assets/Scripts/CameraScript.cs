0b0f570b9574fea7da980ba18f6f94f651a0763d
Nekot94 <nekomato9@gmail.com>
2017-06-19 12:18:39 +0300

    123

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player; 
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.position = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y,
+            transform.position.z);
+	}
+}
---
+++ Assets/Scripts/HeroScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody2D myBody;
+    
+
+	void Start ()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+        myBody.AddForce(new Vector2(moveHorizontal, moveVertical));
+
+    }
+}
